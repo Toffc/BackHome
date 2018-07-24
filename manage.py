331c3184449e811e70b412403ce70f3240605644
Toffc <chenby36@mail2.sysu.edu.cn>
2018-07-25 05:31:38 +0800

    主界面修改
    删除了游戏部分函数

--- manage.py
+++ manage.py
@@ -7,27 +7,3 @@ from bin.main import main
 if __name__ == '__main__':
 
     main()
-
-    # '''
-    # 开始界面
-    # '''
-    # while running:
-    #     ai_settings.screen.blit(ui_back, (0, 0))
-    #     ui.show(ai_settings, stats)
-    #     if stats.function == 1:
-    #         break
-    #         # f1(ai_settings, ui_back, stats)
-    #     elif stats.function == 2:
-    #         f2(ai_settings, ui_back, stats)
-    #     elif stats.function == 3:
-    #         f3(ai_settings, ui_back, stats, v)
-    #     elif stats.function == 4:
-    #         f4(ai_settings, ui_back, stats)
-    #     pygame.display.update()
-
-
-    # endless()
-
-    # '''
-    # 飞机大战
-    # '''
